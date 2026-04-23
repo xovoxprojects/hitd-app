f71e46a88a0911d3a8319461564a206da2b9134c
xovoxprojects <xovoxclips@gmail.com>
2026-04-23 20:21:25 +0200

    fix(auth): add allowDangerousEmailAccountLinking to Google provider to prevent OAuthAccountNotLinked error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     EmailProvider({
       server: process.env.EMAIL_SERVER,
